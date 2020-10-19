21e5924943ff1c53ed447198d2b2c681c5f8b5d3
dhairyaattri <dhairyaattri16@gmail.com>
2020-10-19 20:09:21 +0530

    first commit

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode']
+}
+</script>
+
+<style scoped>
+button {
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #88b77b;
+  border: 1px solid #88b77b;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #88b77b;
+  border-color: #88b77b;
+}
+
+.flat {
+  background-color: transparent;
+  color: #88b77b;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color:white;
+}
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <header><h1>{{ title }}</h1></header>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 5rem;
+  background-color: #88b77b;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+header h1 {
+  color: white;
+  margin: 0;
+  font-weight: 200;
+}
+
+@media (min-width: 320px) {
+  header h1 {
+    font-size: 22px;
+  }
+}
+</style>
